0080bc8789e2198ff4770e0430452ddeaae4a066
Catalin <catalin.es@hotmail.com>
2024-08-25 14:56:28 +0300

    fin

---
+++ src/services/api.jsx
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const KEY = 'fbdc642376dc1e27fcf8ce266154f176';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+axios.defaults.params = { api_key: KEY };
+
+export async function fetchTrending() {
+  const response = await axios.get('trending/movie/day');
+  return response.data.results;
+}
+export async function fetchSearchMovieByWord(query) {
+  const response = await axios(
+    `search/movie?query=${query}&include_adult=false&language=en-US&page=1`
+  );
+  return response.data.results;
+}
+export async function fetchMovieDetails(movie_id) {
+  const response = await axios(`movie/${movie_id}?language=en-US`);
+  return response.data;
+}
+
+export async function fetchMovieCast(movie_id) {
+  const response = await axios(`movie/${movie_id}/credits?language=en-US`);
+  return response.data.cast;
+}
+export async function fetchMovieReviews(movie_id) {
+  const response = await axios(
+    `movie/${movie_id}/reviews?language=en-US&page=1`
+  );
+  return response.data.results;
+}
